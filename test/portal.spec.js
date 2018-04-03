afd1118f4c07accf4aa102cc83d9a9e3ce0e6df7
Chen Ze <chenze@wexfin.com>
2018-04-03 13:56:48 +0800

    $type($scope): $subject

--- test/portal.spec.js
+++ test/portal.spec.js
@@ -2,6 +2,6 @@ import { mount, shallow } from 'avoriaz'
 import PortalTestComponent from './portal.test.vue'
 
 it('should render portal component correctly', () => {
-  const wrapper = shallow(PortalTestComponent)
+  const wrapper = mount(PortalTestComponent)
   expect(wrapper.html()).toMatchSnapshot()
 })
